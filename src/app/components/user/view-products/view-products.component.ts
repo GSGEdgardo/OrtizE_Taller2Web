0e6a077dcc4a7a2cce818a5606a320ae4ced167b
Edgardo Ortiz <edgardo.ortiz@alumnos.ucn.cl>
2024-06-28 23:14:15 -0400

    fix: The search bar now updates results in real-time as the user types.

--- src/app/components/user/view-products/view-products.component.ts
+++ src/app/components/user/view-products/view-products.component.ts
@@ -4,6 +4,7 @@ import { ProductService } from 'src/app/services/product.service';
 import { Product } from 'src/app/models/product';
 import { PurchaseService } from 'src/app/services/purchase.service';
 import { AccountService } from 'src/app/services/account.service';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-view-products',
@@ -18,6 +19,8 @@ export class ViewProductsComponent implements OnInit {
   showModal: boolean = false;
   selectedProduct: any = null;
   totalProducts: number = 0;
+  filteredProducts: Product[] = [];
+  noResultsMessage: string = '';
 
   constructor(
     private productService: ProductService,
@@ -32,33 +35,36 @@ export class ViewProductsComponent implements OnInit {
 
   ngOnInit(): void {
     this.loadProducts();
-  }
 
-  loadProducts(): void {
-    this.productService.getAvailableProducts(this.currentPage, this.pageSize).subscribe({
-      next: (data) => this.products = data,
-      error: (err) => console.error(err)
+    this.searchForm.get('query')?.valueChanges.pipe(
+      debounceTime(300)
+    ).subscribe(query => {
+      this.searchProducts(query);
     });
   }
 
-  loadTotalProducts(): void {
-    this.productService.getAvailableProducts(1, this.pageSize).subscribe({
+  loadProducts(): void {
+    this.productService.getAvailableProducts(1, 10).subscribe({
       next: (data) => {
-        this.totalProducts = data.length;
+        this.products = data;
+        this.filteredProducts = data;
+        this.noResultsMessage = data.length === 0 ? 'No se encontraron productos' : '';
       },
       error: (err) => console.error(err)
     });
   }
 
-  searchProducts(): void {
-    const query = this.searchForm.get('query')?.value.trim();
-    if (!query) {
-      this.loadProducts();
+  searchProducts(query: string): void {
+    const lowerCaseQuery = query.toLowerCase();
+    if (query) {
+      this.filteredProducts = this.products.filter(product =>
+        product.name.toLowerCase().includes(lowerCaseQuery) ||
+        product.productType.type.toLowerCase().includes(lowerCaseQuery)
+      );
+      this.noResultsMessage = this.filteredProducts.length === 0 ? 'No se encontraron productos con esos criterios de búsqueda' : '';
     } else {
-      this.productService.searchAvailableProducts(query, this.currentPage, this.pageSize).subscribe({
-        next: (data) => this.products = data,
-        error: (err) => console.error(err)
-      });
+      this.filteredProducts = this.products;
+      this.noResultsMessage = '';
     }
   }
 
